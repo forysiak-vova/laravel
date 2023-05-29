35281749100dce74b26e82ea92c549dac30f903c
forysiak-vova <volodja.forus@gmail.com>
2023-05-29 21:31:04 +0200

    add new project

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\InvoicesController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', function () {
+    return view('index');
+});
+// Route::get('faktury', function () {
+//     return view('invoices.index');
+// });
+// Route::get('faktury/dodaj', function () {
+//     return view('invoices.create');
+// });
+Route::get('faktury', [InvoicesController::class, 'index']);
+Route::get('faktury/dodaj', [InvoicesController::class, 'create']);
